5b9e33a6a35b6f4baf195add46e3cb33933404de
YoussefAbdallah <youssefabdullah@hotmail.com>
2020-09-13 10:33:29 +0200

    Create requests migration

---
+++ database/migrations/2020_09_13_081434_create_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity')->default(1);
+            $table->text('description')->nullable();
+            $table->boolean('approved')->default(false);
+            $table->enum('type', array('sell', 'loan'));
+            $table->float('price')->default(0);
+            $table->float('price_per_hour')->default(0);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}

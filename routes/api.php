ac1f249459a72ad1544bb2a2487ee7967a9df13b
YoussefAbdallah <youssefabdullah@hotmail.com>
2020-09-24 16:27:09 +0200

    add auth middleware to all the routes

--- routes/api.php
+++ routes/api.php
@@ -21,93 +21,94 @@ use App\Http\Controllers\Auth\FacebookController;
 |
 */
 
+Route::group(['middleware' => 'auth:api'], function() {
+    /* Requests Routes */
+
+    Route::apiResource('requests', 'UserRequestController');
+    Route::put('requests/{request}/approve', 'UserRequestController@approve')->name('requests.approve');
+
+    /* Cart Routes */
+
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::post('cart', 'CartController@store')->name('cart.store');
+    Route::put('cart/{rowid}', 'CartController@update')->name('cart.update');
+    Route::delete('cart/{rowid}', 'CartController@destroy')->name('cart.destroy');
+    Route::get('emptycart', function() {
+        Cart::restore('username');
+        Cart::destroy();
+        Cart::store('username');
+    });
+
+    /* Checkout routes */
+
+    Route::get('checkout', 'CheckoutController@index')->name('checkout.index');
+    Route::post('checkout', 'CheckoutController@store')->name('checkout.store');
+
+    /* Complaints routes */
+
+    Route::apiResource('complaints', 'ComplaintController');
+
+    /* Orders Routes */
+
+    Route::apiResource('orders', 'OrderController');
+    Route::put('orders/updatestatus/{order}', 'OrderController@updateOrderStatus')->name('orders.updateOrderStatus');
+
+    /* Advertisements Routes */
+
+    Route::apiResource('advertisements', 'AdvertisementController');
+
+    /* Order Feedback Routes */
+
+    Route::apiResource('feedbacks', 'OrderFeedbackController');
+
+    /* Home Route */
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    /* User routes */
+
+    Route::apiResource('users', 'UserController');
+
+    /* Subscriptions routes */
+
+    Route::post('/subscriptions', 'SubscriptionController@store')->name('subscriptions.store');
+    /* favourite list routes
+
+    Route::group(['prefix'=>'users'], function (){
+        Route::apiResource('{users}/favouriteList', FavouriteList::class);
+    });
+    */
+
+    /* categories routes*/
+
+    Route::apiResource('/category', CategoryController::class);
+
+    /* subcategories routes*/
+
+    Route::group(['prefix'=>'category'], function () {
+        Route::apiResource('/{category}/subcategory', SubcategoryController::class);
+    });
+
+
+    /* Products routes*/
+
+    Route::group(['prefix'=>'category/{category}/subcategory'], function () {
+        Route::apiResource('{subcategory}/products', ProductController::class);
+    });
+
+
+
+    /* comments routes */
+
+    Route::group(['prefix'=>'category/{category}/subcategory/{subcategory}/products'], function (){
+        Route::apiResource('{products}/comments', CommentController::class);
+    });
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-/* Requests Routes */
-
-Route::apiResource('requests', 'UserRequestController');
-Route::put('requests/{request}/approve', 'UserRequestController@approve')->name('requests.approve');
-
-/* Cart Routes */
-
-Route::get('cart', 'CartController@index')->name('cart.index');
-Route::post('cart', 'CartController@store')->name('cart.store');
-Route::put('cart/{rowid}', 'CartController@update')->name('cart.update');
-Route::delete('cart/{rowid}', 'CartController@destroy')->name('cart.destroy');
-Route::get('emptycart', function() {
-    Cart::restore('username');
-    Cart::destroy();
-    Cart::store('username');
-});
-
-/* Checkout routes */
-
-Route::get('checkout', 'CheckoutController@index')->name('checkout.index');
-Route::post('checkout', 'CheckoutController@store')->name('checkout.store');
-
-/* Complaints routes */
-
-Route::apiResource('complaints', 'ComplaintController');
-
-/* Orders Routes */
-
-Route::apiResource('orders', 'OrderController');
-Route::put('orders/updatestatus/{order}', 'OrderController@updateOrderStatus')->name('orders.updateOrderStatus');
-
-/* Advertisements Routes */
-
-Route::apiResource('advertisements', 'AdvertisementController');
-
-/* Order Feedback Routes */
-
-Route::apiResource('feedbacks', 'OrderFeedbackController');
-
-/* Home Route */
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-/* User routes */
-
-Route::apiResource('users', 'UserController');
-
-/* Subscriptions routes */
-
-Route::post('/subscriptions', 'SubscriptionController@store')->name('subscriptions.store');
-/* favourite list routes
-
-Route::group(['prefix'=>'users'], function (){
-    Route::apiResource('{users}/favouriteList', FavouriteList::class);
-});
- */
-
-/* categories routes*/
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::apiResource('/category', CategoryController::class);
-});
-
-/* subcategories routes*/
-
-Route::group(['prefix'=>'category'], function () {
-    Route::apiResource('/{category}/subcategory', SubcategoryController::class);
-});
-
-
-/* Products routes*/
-
-Route::group(['prefix'=>'category/{category}/subcategory'], function () {
-    Route::apiResource('{subcategory}/products', ProductController::class);
-});
-
-
-
-/* comments routes */
-
-Route::group(['prefix'=>'category/{category}/subcategory/{subcategory}/products'], function (){
-    Route::apiResource('{products}/comments', CommentController::class);
-});
 
 /* facebook route */
 Route::post('auth/facebook', 'Auth\FacebookController@loginFromToken');
